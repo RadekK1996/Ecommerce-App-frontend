c8e64cdfbfbf61564f00940dc6020b444bd3395d
Radosław Kowalski <ladekesl@gmail.com>
2023-10-23 19:59:16 +0200

    Add elementary structure of the  Register component

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,3 +1,30 @@
 export const AuthPage = () => {
-  return <div>Auth</div>;
+  return (
+    <div className="auth">
+      <Register /> <Login />
+    </div>
+  );
+};
+
+const Register = () => {
+  return (
+    <div className="auth-container">
+      <form>
+        <h2>Register</h2>
+        <div className="form-group">
+          <label htmlFor="username">Username:</label>
+          <input type=" text" id="username" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
+
+const Login = () => {
+  return <div>Login</div>;
 };
