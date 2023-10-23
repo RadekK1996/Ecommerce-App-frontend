48b2db7c1285fb99702911bd0d774d8e4b84727a
Radosław Kowalski <ladekesl@gmail.com>
2023-10-23 19:06:53 +0200

    Add basic react-router-dom navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import React from "react";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import { NavBar } from "./components/navbar";
 
 function App() {
-  return <h1>Main Page</h1>;
+  return (
+    <div className="App">
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" />
+          <Route path="/auth" />
+          <Route path="/checkout" />
+          <Route path="/purchased-items" />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
