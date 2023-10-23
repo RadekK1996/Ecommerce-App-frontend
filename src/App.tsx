a7961f2dd6d5ff0b866d9ede8d253f6c25985da6
Radosław Kowalski <ladekesl@gmail.com>
2023-10-23 19:40:07 +0200

    Create basic structure of pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import { NavBar } from "./components/navbar";
+import { ShopPage } from "./pages/shop";
+import { AuthPage } from "./pages/auth";
+import { CheckoutPage } from "./pages/checkout";
+import { PurchasedItemsPage } from "./pages/purchased-items";
 
 function App() {
   return (
@@ -8,10 +12,13 @@ function App() {
       <Router>
         <NavBar />
         <Routes>
-          <Route path="/" />
-          <Route path="/auth" />
-          <Route path="/checkout" />
-          <Route path="/purchased-items" />
+          <Route path="/" element={<ShopPage></ShopPage>} />
+          <Route path="/auth" element={<AuthPage></AuthPage>} />
+          <Route path="/checkout" element={<CheckoutPage></CheckoutPage>} />
+          <Route
+            path="/purchased-items"
+            element={<PurchasedItemsPage></PurchasedItemsPage>}
+          />
         </Routes>
       </Router>
     </div>
