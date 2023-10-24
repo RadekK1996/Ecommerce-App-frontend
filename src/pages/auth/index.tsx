e4817dd2b34b7a3b9841ed0f5360b593c64cb19c
Radosław Kowalski <ladekesl@gmail.com>
2023-10-24 17:53:36 +0200

    Implement user login funcionality in the Login  component

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { SyntheticEvent, useState } from "react";
 import { UserErrors } from "../../errors";
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
 
 export const AuthPage = () => {
   return (
@@ -11,8 +13,8 @@ export const AuthPage = () => {
 };
 
 const Register = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
 
   const handleSubmit = async (e: SyntheticEvent) => {
     e.preventDefault();
@@ -59,5 +61,61 @@ const Register = () => {
 };
 
 const Login = () => {
-  return <div>Login</div>;
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [, setCookies] = useCookies(["access_token"]);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: SyntheticEvent) => {
+    e.preventDefault();
+    try {
+      const result = await axios.post("http://localhost:3001/user/login", {
+        username,
+        password,
+      });
+      setCookies("access_token", result.data.token);
+      localStorage.setItem("userID", result.data.userID);
+      navigate("/");
+    } catch (err) {
+      let errorMessage: string = "";
+
+      switch (err.response.data.type) {
+        case UserErrors.NO_USER_FOUND:
+          errorMessage = "No user found";
+          break;
+        case UserErrors.WRONG_CREDENTIALS:
+          errorMessage = "Wrong credentials";
+          break;
+        default:
+          errorMessage = "Something went wrong";
+      }
+      alert("Error:" + errorMessage);
+    }
+  };
+  return (
+    <div className="auth-container">
+      <form onSubmit={handleSubmit}>
+        <h2>Login</h2>
+        <div className="form-group">
+          <label htmlFor="username">Username:</label>
+          <input
+            type=" text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 };
