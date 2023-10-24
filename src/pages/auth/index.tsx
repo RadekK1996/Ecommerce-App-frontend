dc797d5785daf2afed42ce680e4210c7ab948fe2
Radosław Kowalski <ladekesl@gmail.com>
2023-10-24 17:27:17 +0200

    Implement user registration funcionality in the Register component

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,3 +1,7 @@
+import axios from "axios";
+import { SyntheticEvent, useState } from "react";
+import { UserErrors } from "../../errors";
+
 export const AuthPage = () => {
   return (
     <div className="auth">
@@ -7,17 +11,46 @@ export const AuthPage = () => {
 };
 
 const Register = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e: SyntheticEvent) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:3001/user/register", {
+        username,
+        password,
+      });
+      alert("Registration completed. Now Login.");
+    } catch (err) {
+      if (err?.response?.data?.type === UserErrors.USERNAME_ALREADY_EXIST) {
+        alert("Error: Username already exists");
+      } else {
+        alert("Error: Something went wrong");
+      }
+    }
+  };
   return (
     <div className="auth-container">
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>Register</h2>
         <div className="form-group">
           <label htmlFor="username">Username:</label>
-          <input type=" text" id="username" />
+          <input
+            type=" text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </div>
         <div className="form-group">
           <label htmlFor="password">Password:</label>
-          <input type="password" id="password" />
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
         <button type="submit">Register</button>
       </form>
