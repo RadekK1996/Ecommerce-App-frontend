6d1e6723453b7ed6b95951718b9c6482b7d8dcf3
Radosław Kowalski <ladekesl@gmail.com>
2023-11-07 18:43:59 +0100

    Implement display products in shop page

--- src/pages/shop/index.tsx
+++ src/pages/shop/index.tsx
@@ -1,3 +1,21 @@
+import { useGetProducts } from "../../hooks/useGetProducts";
+import { Product } from "../../types/types";
+
 export const ShopPage = () => {
-  return <div>Shop</div>;
+  const { products } = useGetProducts();
+  return (
+    <div className="shop">
+      <div className="products">
+        {products !== null ? (
+          products.map((product: Product) => (
+            <div key={product._id}>
+              {product.productName} {product.price}
+            </div>
+          ))
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
+    </div>
+  );
 };
