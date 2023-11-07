dbe1b925355069f61c36031a6faf382aecdff33e
Radosław Kowalski <ladekesl@gmail.com>
2023-11-07 18:39:19 +0100

    Added optional chaining to handle potential undefined values in error response data.

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -23,7 +23,7 @@ const Login = () => {
     } catch (err) {
       let errorMessage: string = "";
 
-      switch (err.response.data.type) {
+      switch (err?.response?.data?.type) {
         case UserErrors.NO_USER_FOUND:
           errorMessage = "No user found";
           break;
