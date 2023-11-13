6ab163721ac676afd3d163744d8b5e71430583cc
Radosław Kowalski <ladekesl@gmail.com>
2023-11-13 20:22:07 +0100

    Implement shopping cart functionality: add, display, and manage product quantities. Show total order amount in checkout. Add 'Continue Shopping' and 'Checkout' buttons.

--- src/pages/checkout/cart-item.tsx
+++ src/pages/checkout/cart-item.tsx
@@ -1,17 +1,32 @@
+import { useContext } from "react";
 import { IProduct } from "../../types/types";
+import { IShopContext, ShopContext } from "../../context/shop-context";
 
 interface Props {
   product: IProduct;
 }
 
 export const CartItem = (props: Props) => {
+  const { getCartItemCount, addToCart, removeFromCart, updateCartItemCount } =
+    useContext<IShopContext>(ShopContext);
   const { _id, productName, price, imageURL } = props.product;
+
+  const cartItemCount = getCartItemCount(_id);
   return (
     <div className="cartItem">
       <img src={imageURL} alt="" />
       <div className="description">
         <h3>{productName}</h3>
         <p>Price: ${price}</p>
+      </div>
+      <div className="count-handler">
+        <button onClick={() => removeFromCart(_id)}>-</button>
+        <input
+          type="number"
+          value={cartItemCount}
+          onChange={(e) => updateCartItemCount(Number(e.target.value), _id)}
+        />
+        <button onClick={() => addToCart(_id)}>+</button>
       </div>
     </div>
   );
--- src/pages/checkout/index.tsx
+++ src/pages/checkout/index.tsx
@@ -5,27 +5,39 @@ import { IShopContext, ShopContext } from "../../context/shop-context";
 import { CartItem } from "./cart-item";
 
 import "./styles.css";
+import { useNavigate } from "react-router-dom";
 
 export const CheckoutPage = () => {
-  const { getCartItemCount } = useContext<IShopContext>(ShopContext);
+  const { getCartItemCount, getTotalCountAmount } =
+    useContext<IShopContext>(ShopContext);
   const { products } = useGetProducts();
+  const navigate = useNavigate();
+
+  const totalAmount = getTotalCountAmount();
+
   return (
     <div className="cart">
       <div>
         <h1>Your Cart Items</h1>
       </div>
       <div className="cart">
-        {products ? (
+        {products &&
           products.map((product: IProduct) => {
             if (getCartItemCount(product._id) !== 0) {
               return <CartItem key={product._id} product={product} />;
             }
             return null;
-          })
-        ) : (
-          <p>No products available</p>
-        )}
+          })}
       </div>
+      {totalAmount > 0 ? (
+        <div className="checkout">
+          <p>Subtotal: {totalAmount.toFixed(2)}$</p>
+          <button onClick={() => navigate("/")}>Continue Shopping</button>
+          <button>Checkout</button>
+        </div>
+      ) : (
+        <h1>Your Shopping Cart is empty</h1>
+      )}
     </div>
   );
 };
