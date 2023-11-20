01bdb8707b06c03fddb735c7f1a1385699799d3c
Radosław Kowalski <ladekesl@gmail.com>
2023-11-20 19:25:35 +0100

    Add PurchasedItemsPage component

--- src/context/shop-context.tsx
+++ src/context/shop-context.tsx
@@ -119,6 +119,7 @@ export const ShopContextProvider = (props) => {
 
       setCartItems({});
       fetchAvailableMoney();
+      fetchPurchasedItems();
       navigate("/");
     } catch (err) {
       console.log(err);
--- src/pages/purchased-items/index.tsx
+++ src/pages/purchased-items/index.tsx
@@ -1,3 +1,30 @@
+import { useContext } from "react";
+import { IShopContext, ShopContext } from "../../context/shop-context";
+import "./styles.css";
+
 export const PurchasedItemsPage = () => {
-  return <div>PurchasedItems</div>;
+  const { purchasedItems, addToCart, getCartItemCount } =
+    useContext<IShopContext>(ShopContext);
+
+  return (
+    <div className="purchased-items-page">
+      <h1>Previously Purchased Items</h1>
+
+      <div className="purchased-items">
+        {purchasedItems.map((item) => {
+          const cartItemCount = getCartItemCount(item._id);
+          return (
+            <div className="item" key={item._id}>
+              <h3>{item.productName}</h3>
+              <img src={item.imageURL} alt="" />
+              <p>${item.price}</p>
+              <button onClick={() => addToCart(item._id)}>
+                Purchase Again {cartItemCount > 0 && <> ({cartItemCount})</>}
+              </button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
