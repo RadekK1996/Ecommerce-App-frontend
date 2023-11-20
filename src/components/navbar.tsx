80bfa0de6de00c8f9da671a3118d7242e22b7399
Radosław Kowalski <ladekesl@gmail.com>
2023-11-20 19:39:57 +0100

    Added basic logic for logout button.

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,9 +3,16 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import { useContext } from "react";
 import { IShopContext, ShopContext } from "../context/shop-context";
+import { useCookies } from "react-cookie";
 
 export const NavBar = () => {
   const { availableMoney } = useContext<IShopContext>(ShopContext);
+  const [, setCookies] = useCookies(["access_token"]);
+
+  const logout = () => {
+    localStorage.clear();
+    setCookies("access_token", null);
+  };
   return (
     <div className="navbar">
       <div className="navbar-title">
@@ -16,6 +23,9 @@ export const NavBar = () => {
         <Link to="/purchased-items">Purchases</Link>
         <Link to="/checkout">
           <FontAwesomeIcon icon={faShoppingCart} />
+        </Link>
+        <Link to="/auth" onClick={logout}>
+          Logout
         </Link>
         <span>${availableMoney.toFixed(2)}</span>
       </div>
